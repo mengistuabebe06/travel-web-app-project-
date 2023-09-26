6d6c787ca2756054515b469609096e743b18509a
mengistuabebe06 <mengistuabebe06@gmail.com>
2023-09-26 16:55:17 +0300

    model migration

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    # id = models.UUIDField()
+    name = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='media')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer  = models.BooleanField(default=False)
--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from travello.models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    
+    # dest1 = Destination()
+    # dest1.name = "mengistu"
+    # dest1.img = "destination_1.jpg"
+    # dest1.price = 456
+    # dest1.desc = "adadasdasdadasdadsada"
+    # dest1.offer = False
+    
+    # dest2 = Destination()
+    # dest2.name = "Abebe"
+    # dest2.img = "destination_2.jpg"
+    # dest2.price = 789
+    # dest2.desc = "adadasdasdadasdadsada"
+    # dest2.offer = True
+    
+    # dest3 = Destination()
+    # dest3.name = "Legesse"
+    # dest3.img = "destination_3.jpg"
+    # dest3.price = 213
+    # dest3.desc = "adadasdasdadasdadsada"
+    # dest3.offer = False
+    
+    # dests = [dest1,dest2,dest3]
+    dest = Destination.objects.all().values()
+    
+    return render(request,'index.html', {'dests':dest})
